fc3e7716f6fc08f80992711f0b3c1ff34207b5c0
rafael lefort <rafael.lefort@hotmail.com>
2025-11-26 13:51:19 +0100

    Retrait de constants.dart contenant des clés sensibles

--- lib/services/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-const String RAWG_API_KEY = '5340b348e4a14f258ddbc8b41f3ffb3f';
-
-const String SUPABASE_ANON_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxb3Blc3B4ZW5qcnVsbGdhcGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNzI3NTEsImV4cCI6MjA3OTY0ODc1MX0.hlQPrdArd_tFbDI5Vf9oTL1fdTYWRd6Q8tCbHLyFl70';
-
-// https://api.rawg.io/api/games?platforms=6&key=5340b348e4a14f258ddbc8b41f3ffb3f
